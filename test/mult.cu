6af32c7236e5d774128f1177432ce834f0d0c7c6
Jingcheng Liu <liuexp@gmail.com>
2013-02-18 18:40:19 +0800

    updated experimental results

--- test/mult.cu
+++ test/mult.cu
@@ -51,14 +51,19 @@ void my_read_matrix(){
 	diff = difftime(time1, time0);
 	printf("Reading %d lines takes %.3f\n", cnt, diff);
 }
+void reportTime(clock_t t0){
+	printf("----ELAPSED TIME: %.8fs\n", ((double)clock() - t0)/CLOCKS_PER_SEC);
+}
 
 int main(void)
 {
 	time_t t0,t1;
 	double diff;
+	clock_t tt0=clock();
 	cudaSetDevice(0);
 
 	my_read_matrix();
+	reportTime(tt0);
 
 	cusp::coo_matrix<int, float, cusp::device_memory> A(B);
 	cusp::array1d<float, cusp::device_memory> x(A.num_cols, 1);
@@ -67,24 +72,39 @@ int main(void)
 
 	// for each iteration,  y<-A*x, x<-z, x<-D*y + x
 	for(int i=0;i<niter;i++){
-		clock_t tc;
-		tc = clock();
+		clock_t tc, ti;
+
+		ti = tc = clock();
 		cusp::multiply(A, x, y);
+		diff = ((float)clock() - tc)/CLOCKS_PER_SEC;
+		printf("multiplication takes %.5f\n",diff);
+
+		tc = clock();
 		x=z;
+		diff = ((float)clock() - tc)/CLOCKS_PER_SEC;
+		printf("assignment takes %.5f\n",diff);
+
+		tc = clock();
 		cusp::blas::axpy(y,x,DAMPINGFACTOR);
 		diff = ((float)clock() - tc)/CLOCKS_PER_SEC;
+		printf("blas::axpy takes %.5f\n",diff);
+
+		diff = ((float)clock() - ti)/CLOCKS_PER_SEC;
 		printf("iteration takes %.5f\n",diff);
 	}
+	reportTime(tt0);
 	time(&t0);
 	cusp::array1d<float, cusp::host_memory> yy(x);
 	time(&t1);
 	diff = difftime(t1,t0);
 	printf("copying host takes %.3f\n",diff);
+	reportTime(tt0);
 	time(&t0);
 
 	std::vector<float> yyy(yy.begin(), yy.end());
 	for(int i=0;i<30;i++)
 		printf("%.8f\n",yyy[i]);
+	reportTime(tt0);
 	return 0;
 }
 
